3ad5c223d65995ad768464829aa023ed6834a5ff
Jonathan <jonathan_hof@outlook.de>
2022-12-15 17:28:07 +0100

    register Form-Django test

--- ToDoListe/views.py
+++ ToDoListe/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, FileResponse
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'ToDoListe/login.html', {})
+    register_Form = UserCreationForm()
+    return render(request, 'ToDoListe/login.html', {'registerForm': register_Form,})
 
