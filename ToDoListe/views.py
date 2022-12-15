be68804c415733a99a6ba106864d05b1ea3bc696
Jonathan <jonathan_hof@outlook.de>
2022-12-15 17:41:14 +0100

    make register-Form usable

--- ToDoListe/views.py
+++ ToDoListe/views.py
@@ -7,5 +7,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     register_Form = UserCreationForm()
+
+    if request.method == 'POST':
+        if request.POST["operator"] == "register":
+            request.POST.pop("operator")
+            register_Form = UserCreationForm(request.POST)
+            if register_Form.is_valid():
+                register_Form.save()
+
+
     return render(request, 'ToDoListe/login.html', {'registerForm': register_Form,})
 
