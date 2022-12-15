e9bdbe9fca8ad32be63b8945c8af0bfa422ddbf1
Jonathan <jonathan_hof@outlook.de>
2022-12-15 16:41:34 +0100

    template Einbindung

--- website/settings.py
+++ website/settings.py
@@ -16,8 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-# Subdirs
-ToDoListe_TEMPLATE_DIR = os.path.join(BASE_DIR, 'ToDoListe/templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -58,7 +57,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ToDoListe_TEMPLATE_DIR],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
